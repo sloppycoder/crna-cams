d564d5dd9425f4e52c308b1936241038e34da653
Li Lin <guru.lin@gmail.com>
2017-08-15 02:27:11 +0800

    use icons on tab bar

--- src/components/MainTabNavigator.js
+++ src/components/MainTabNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Platform } from 'react-native';
 import { TabNavigator, TabBarBottom } from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
 
 import AccountListScreen from '../screens/AccountListScreen';
 import SettingsScreen from '../screens/SettingsScreen';
@@ -8,28 +9,45 @@ import SettingsScreen from '../screens/SettingsScreen';
 export default TabNavigator(
   {
     Home: {
-      screen: AccountListScreen,
+      screen: AccountListScreen
     },
     Settings: {
-      screen: SettingsScreen,
-    },
+      screen: SettingsScreen
+    }
   },
   {
     navigationOptions: ({ navigation }) => ({
       tabBarIcon: ({ focused }) => {
         const { routeName } = navigation.state;
+        let iconName;
+        switch (routeName) {
+          case 'Home':
+            iconName =
+              Platform.OS === 'ios'
+                ? `ios-home${focused ? '' : '-outline'}`
+                : 'md-home';
+            break;
+          case 'Settings':
+            iconName =
+              Platform.OS === 'ios'
+                ? `ios-settings${focused ? '' : '-outline'}`
+                : 'md-settings';
+        }
         return (
-          <Text>
-            {routeName.concat(focused ? '*' : '')}
-          </Text>
+          <Ionicons
+            name={iconName}
+            size={28}
+            style={{ marginBottom: -3 }}
+            color={focused ? '#030303' : '#ccc'}
+          />
         );
-      },
+      }
     }),
     tabBarComponent: TabBarBottom,
     tabBarPosition: 'bottom',
     tabBarOptions: { showLabel: false },
     animationEnabled: false,
     swipeEnabled: false,
-    mode: 'modal',
+    mode: 'modal'
   }
 );
